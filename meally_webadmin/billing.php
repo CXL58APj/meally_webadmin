fd33d9fd0939f4445b1f67042b82aa3b36eb636a
Jocelyn Vega <40295780+CXL58APj@users.noreply.github.com>
2022-05-13 13:24:46 +0800

    Update billing.php

--- meally_webadmin/billing.php
+++ meally_webadmin/billing.php
@@ -56,7 +56,7 @@
           <h6 class="ps-4 ms-2 text-uppercase text-xs text-white font-weight-bolder opacity-8">Management</h6>
         </li>
         <li class="nav-item">
-          <a class="nav-link text-white " href="billing.php">
+          <a class="nav-link text-white active bg-gradient-primary " href="billing.php">
             <div class="text-white text-center me-2 d-flex align-items-center justify-content-center">
               <i class="material-icons opacity-10">receipt_long</i>
             </div>
@@ -83,7 +83,7 @@
           <h6 class="ps-4 ms-2 text-uppercase text-xs text-white font-weight-bolder opacity-8">System Management</h6>
         </li>
         <li class="nav-item">
-          <a class="nav-link text-white active bg-gradient-primary " href="systemusers.php">
+          <a class="nav-link text-white " href="systemusers.php">
             <div class="text-white text-center me-2 d-flex align-items-center justify-content-center">
               <i class="material-icons opacity-10">person</i>
             </div>
@@ -104,7 +104,8 @@
         <nav aria-label="breadcrumb">
           <ol class="breadcrumb bg-transparent mb-0 pb-0 pt-1 px-0 me-sm-6 me-5">
             <li class="breadcrumb-item text-sm"><a class="opacity-5 text-dark" href="javascript:;">Pages</a></li>
-            <li class="breadcrumb-item text-sm text-dark active" aria-current="page">System Management</li>
+            <li class="breadcrumb-item text-sm text-dark active" aria-current="page">Management</li>
+            <li class="font-weight-bolder mb-0 breadcrumb-item text-sm text-dark active" aria-current="page">Billing</li>
           </ol>
           </nav>
         <div class="collapse navbar-collapse mt-sm-0 mt-2 me-md-0 me-sm-4" id="navbar">
